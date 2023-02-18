21614ce746f258edba12f9d7cba10a458f865654
Nikola908 <nikolajovanovic109@gmail.com>
2023-02-18 20:24:34 +0100

    Commit

---
+++ Licitacija/Licitacija1/Licitacija1/DTOs/DokumentUpdateDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Licitacija1.DTOs
+{
+    public class DokumentUpdateDTO
+    {
+        /// <summary>
+        /// Identifikator dokumenta
+        /// </summary>
+        public Guid dokumentId { get; set; }
+        /// <summary>
+        /// Identifikator lictacije
+        /// </summary>
+        public Guid LicitacijaID { get; set; }
+        /// <summary>
+        /// naziv dokumenta
+        /// </summary>
+        public string NazivDokumenta { get; set; }
+        ///P pravno lice, F fizicko lice
+        public string vrstaPodnosiocaDokumenta { get; set; }
+        /// <summary>
+        ///  datum
+        /// </summary>
+        public DateTime datum { get; set; }
+    }
+}
---
+++ Licitacija/Licitacija1/Licitacija1/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Licitacija1.Entities;
+
+namespace Licitacija1.Data
+{
+    public class AppDbContext : DbContext
+    {
+       public AppDbContext(DbContextOptions<AppDbContext> opt) : base (opt)
+        {
+
+        }
+         
+        public DbSet<LicitacijaModel> Licitacije { get; set; }
+        public DbSet<DokumentModel> Dokumenti { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<LicitacijaModel>().HasMany(x => x.dokumentFizickaLica).WithOne(x => x.licitacija).HasForeignKey(x => x.licitacijaID);
+
+        }
+    }
+}
